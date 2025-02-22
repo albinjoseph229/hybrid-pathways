abae2397335910c52af8db8c5fa293538eca62a5
albz007 <albinjoseph229@gmail.com>
2025-02-22 11:14:29 +0530

    Added More pages in admin module.

---
+++ src/app/components/admin-components/admin-view-updates/admin-view-updates.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-view-updates',
+  imports: [],
+  templateUrl: './admin-view-updates.component.html',
+  styleUrl: './admin-view-updates.component.css'
+})
+export class AdminViewUpdatesComponent {
+
+}
