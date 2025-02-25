e61fe3ba1d174c311ab69dfc449ddeff9306b662
MUHAMMED NIHAL <nihal.offcl@gmail.com>
2025-02-25 10:57:18 +0530

    header componenet error fix

--- src/app/components/hybrid-overseas/jobseeker-components/header-jobseeker/header-jobseeker.component.ts
+++ src/app/components/hybrid-overseas/jobseeker-components/header-jobseeker/header-jobseeker.component.ts
@@ -1,11 +1,14 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header-jobseeker',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './header-jobseeker.component.html',
   styleUrl: './header-jobseeker.component.css'
 })
 export class HeaderJobseekerComponent {
+
+  isMenuOpen = false;
 
 }
